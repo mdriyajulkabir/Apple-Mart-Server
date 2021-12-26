1c66f81eeef790ee8fce33ed2015077e791893db
MD Riyajul Kabir Rakib <mdriyajulkabir@gmail.com>
2021-12-26 20:22:25 +0600

    make server environment and get all phone data

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iyyd2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("appleMartShop");
+    const phoneCollection = database.collection("phones");
+    // GET API
+    app.get("/product", async (req, res) => {
+      const cursor = phoneCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Mens suits  server is running");
+});
+
+app.listen(port, () => {
+  console.log("server is running", port);
+});
